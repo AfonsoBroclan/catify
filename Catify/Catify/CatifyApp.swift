556fce3c08087f6ebacb44c5d72c4cbeeb7ab1ed
Afonso Rosa <afonso.rosa@farfetch.com>
2024-06-01 16:48:55 +0100

    Project setup

---
+++ Catify/Catify/CatifyApp.swift
@@ -0,0 +1,20 @@
+//
+//  CatifyApp.swift
+//  Catify
+//
+//  Created by Afonso Rosa on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatifyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
